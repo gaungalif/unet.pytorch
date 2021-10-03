2bd13308dd7acf794aa6ab7abb4b71e7c6a3f7ce
gaungalif <gaungalif@gmail.com>
2021-10-04 00:46:59 +0800

    feat: add initial package

--- train.py
+++ train.py
@@ -1,17 +1,19 @@
+from pathlib import Path
+import logging
 import argparse
+
+import torch
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 from unet.datamodule import BrainMRISegmentationDataModule
 from unet.module import UNet
-from pytorch_lightning.callbacks import ModelCheckpoint
-# import mlflow
-from pathlib import Path
-import torch
-import logging
+import mlflow
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default=None)
-    parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints/')
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--num_workers', type=int, default=2)
     
@@ -23,31 +25,33 @@ if __name__ == "__main__":
     
     # mlflow.set_tracking_uri("http://localhost:54849")
     # mlflow.pytorch.autolog()
-    check_dir = hparams.checkpoint_dir
-    check_dir = Path(check_dir)
+    
     datamod = BrainMRISegmentationDataModule(**dict_args)
-    mobilenetv2 = UNet(**dict_args)
+    unet = UNet(**dict_args)
     
     model_checkpoint = ModelCheckpoint(
-        dirpath=check_dir,
+        dirpath='checkpoints/',
         save_top_k=1,
-        filename="mobilenet_v2-{epoch:02d}-{val_step_loss:.4f}-{val_step_acc:.4f}",
+        filename="UNet-{epoch:02d}-{val_step_loss:.4f}",
         verbose=True,
         monitor='val_step_loss',
         mode='min',
     )
+    
     trainer = pl.Trainer.from_argparse_args(hparams, callbacks=model_checkpoint)
-    # with mlflow.start_run() as run:
-    trainer.fit(mobilenetv2, datamod)
-    trainer.save_checkpoint(check_dir.joinpath('latest.ckpt'))
-    # mobilenetv2.model.state_dict()
+    with mlflow.start_run() as run:
+        trainer.fit(unet, datamod)
+    trainer.save_checkpoint("checkpoints/latest.ckpt")
     
     metrics =  trainer.logged_metrics
-    # metrics['epoch']
-    vacc, vloss, last_epoch = metrics['val_step_acc'], metrics['val_step_loss'], metrics['epoch']
+    vacc, last_epoch = metrics['val_step_acc'], metrics['epoch']
     
-    filename = f'mobilenet_v2-{last_epoch:02d}_acc{vacc:.4f}_loss{vloss:.4f}.pth'
-    saved_filename = str(Path('classify.pytorch/weights').joinpath(filename))
+    filename = f'unet-{last_epoch:02d}_acc{vacc:.4f}.pth'
+    saved_filename = str(Path('weights').joinpath(filename))
     
     logging.info(f"Prepare to save training results to path {saved_filename}")
-    torch.save(mobilenetv2.model.state_dict(), saved_filename)
+    torch.save(unet.model.state_dict(), saved_filename)
+    
+    
+    
+    
