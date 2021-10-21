3694b1f550c34b390015a7ff801afe819fdbf5da
gaungalif <gaungalif@gmail.com>
2021-10-22 01:56:10 +0800

    feat: add start_feat

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from unet.predictor import UNetSegmentation, UNetSegmentationOnnx
 from unet.utils import *
 import fire
 
-def predict(weight: str = './weights/unet-epoch800-loss0.0093.pth', weight_onnx = './weights/unet-epoch800-loss0.0093.pth.onnx', 
-            data_dir = './dataset/brain_mri/', idx = 23, mode='torch'):
+def predict(weight: str = './weights/unet-epoch800-loss0.0093.pth', weight_onnx: str = './weights/unet-epoch800-loss0.0093.pth.onnx', 
+            data_dir: str = './dataset/brain_mri/', idx: int = 23, mode: str = 'torch', start_feat: int = 32 ):
 
     if mode=="torch":
-        model = UNetSegmentation(weight=weight)
+        model = UNetSegmentation(weight=weight,)
     elif mode=="onnx":
         model = UNetSegmentationOnnx(weight=weight_onnx)
     else:
